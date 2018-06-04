d932417d8ac5273eee773e0db7e261d8c6ee7dcb
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-06-04 23:08:46 +0200

    move code to class

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -8,7 +8,61 @@ export class Item {
         this.sellIn = sellIn;
         this.quality = quality;
     }
+    updateItem() {
+        if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (this.quality > 0) {
+                if (this.name != 'Sulfuras, Hand of Ragnaros') {
+                    this.quality = this.quality - 1
+                }
+            }
+        } else {
+            if (this.quality < 50) {
+                this.quality = this.quality + 1
+                if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if (this.sellIn < 11) {
+                        if (this.quality < 50) {
+                            this.quality = this.quality + 1
+                        }
+                    }
+                    if (this.sellIn < 6) {
+                        if (this.quality < 50) {
+                            this.quality = this.quality + 1
+                        }
+                    }
+                }
+            }
+        }
+        if (this.name != 'Sulfuras, Hand of Ragnaros') {
+            this.sellIn = this.sellIn - 1;
+        }
+        if (this.sellIn < 0) {
+            if (this.name != 'Aged Brie') {
+                if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
+                    if (this.quality > 0) {
+                        if (this.name != 'Sulfuras, Hand of Ragnaros') {
+                            this.quality = this.quality - 1
+                        }
+                    }
+                } else {
+                    this.quality = this.quality - this.quality
+                }
+            } else {
+                if (this.quality < 50) {
+                    this.quality = this.quality + 1
+                }
+            }
+        }
+    }
+    getRaw() {
+        return {
+            name:this.name,
+            quality: this.quality,
+            sellIn:this.sellIn,
+        }
+    }
+
 }
+
 
 export class GildedRose {
     items: Array<Item>;
@@ -19,51 +73,10 @@ export class GildedRose {
 
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
+            let item = this.items[i];
+            item.updateItem();
         }
 
-        return this.items;
+        return this.items.map(item=>item.getRaw());
     }
 }
