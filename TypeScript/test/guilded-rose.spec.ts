29c04b1c7cbf4d786899764391386fa3bbe086da
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-06-04 23:02:47 +0200

    finished covering with tests

--- TypeScript/test/guilded-rose.spec.ts
+++ TypeScript/test/guilded-rose.spec.ts
@@ -1,12 +1,98 @@
-import { expect } from 'chai';
-import { Item, GildedRose } from '../app/gilded-rose';
+import {expect} from 'chai';
+import {Item, GildedRose} from '../app/gilded-rose';
+import * as util from "util";
+
+let agedBrie = "Aged Brie";
+let backstagePass = "Backstage passes to a TAFKAL80ETC concert";
+let sulfras = "Sulfuras, Hand of Ragnaros";
+
+function updateMany(gildedRose) {
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+}
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+    it('starting with zero', function () {
+        const gildedRose = new GildedRose([
+            new Item('foo', 0, 0),
+            new Item(agedBrie, 0, 0),
+            new Item(backstagePass, 0, 0),
+            new Item(sulfras, 0, 0),
+        ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('foo');
+
+        expect(items).eql([
+            {"name": "foo", "sellIn": -1, "quality": 0}, {
+                "name": agedBrie,
+                "sellIn": -1,
+                "quality": 2
+            }, {
+                "name": backstagePass,
+                "sellIn": -1,
+                "quality": 0
+            },
+            {"name": sulfras, "sellIn": 0, "quality": 0}])
     });
 
+    it('starting with two', function () {
+        const gildedRose = new GildedRose([
+            new Item('foo', 2, 2),
+            new Item(agedBrie, 2, 2),
+            new Item(backstagePass, 2, 2),
+            new Item(sulfras, 2, 2),
+        ]);
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        const items = gildedRose.updateQuality();
+
+        expect(items).eql([{"name": "foo", "sellIn": -1, "quality": 0}, {
+            "name": "Aged Brie",
+            "sellIn": -1,
+            "quality": 6
+        }, {
+            "name": "Backstage passes to a TAFKAL80ETC concert",
+            "sellIn": -1,
+            "quality": 0
+        }, {"name": "Sulfuras, Hand of Ragnaros", "sellIn": 2, "quality": 2}])
+    });
+
+
+    it('starting with a lot', function () {
+        const gildedRose = new GildedRose([
+            new Item('foo', 15, 30),
+            new Item(agedBrie, 15, 30),
+            new Item(backstagePass, 15, 30),
+            new Item(sulfras, 15, 30),
+        ]);
+        updateMany(gildedRose);
+        let items = gildedRose.updateQuality();
+
+        expect(items).eql([{"name": "foo", "sellIn": 6, "quality": 21}, {
+            "name": "Aged Brie",
+            "sellIn": 6,
+            "quality": 39
+        }, {
+            "name": "Backstage passes to a TAFKAL80ETC concert",
+            "sellIn": 6,
+            "quality": 43
+        }, {"name": "Sulfuras, Hand of Ragnaros", "sellIn": 15, "quality": 30}])
+        updateMany(gildedRose);
+        items = gildedRose.updateQuality();
+        expect(items).eql([{"name": "foo", "sellIn": -3, "quality": 9}, {
+            "name": "Aged Brie",
+            "sellIn": -3,
+            "quality": 50
+        }, {
+            "name": "Backstage passes to a TAFKAL80ETC concert",
+            "sellIn": -3,
+            "quality": 0
+        }, {"name": "Sulfuras, Hand of Ragnaros", "sellIn": 15, "quality": 30}])
+    });
 });
